c3767620b75f69da1e9d134f21c22f53ccf7de96
PedroMateus26 <pedromateusneto@gmail.com>
2020-12-05 09:30:09 -0300

    Some methods created, reloadData, deleteRoom, roomDetailds, updateRoom created in roomList component

--- front-end/client-room/src/app/room-list/room-list.component.ts
+++ front-end/client-room/src/app/room-list/room-list.component.ts
@@ -1,4 +1,8 @@
+import { RoomService } from './../room.service';
+import { Room } from './../Room';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
-
-  constructor() { }
+  rooms:Observable<Room[]>
+  constructor(private roomService:RoomService, private router:Router) { }
 
   ngOnInit(): void {
+    this.reloadData();
+  }
+
+  reloadData(){
+    this.rooms=this.roomService.getRoomList();
+  }
+
+  deleteRoom(id:number){
+    this.roomService.deleteRoom(id)
+    .subscribe(
+      data=>{
+        console.log(data);
+        this.reloadData();
+      },error=>{
+        console.log(error);
+        
+      }
+    )
+    
+  }
+  roomDetails(id:number){
+    this.router.navigate(["details",id]);
+  }
+  updateRoom(id:number){
+    this.router.navigate(["update",id]);
+
   }
 
 }
